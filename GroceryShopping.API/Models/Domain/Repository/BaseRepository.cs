69a92fed4b4c48ad8f673872d3756d8b44ef2bf0
amina12-aj <bakareaminata@gmail.com>
2022-07-16 21:26:40 +0100

    main

---
+++ GroceryShopping.API/Models/Domain/Repository/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using GroceryShopping.API.DatabaseEF.Context;
+
+
+namespace GroceryShopping.API.Models.Domain.Repository
+{
+   
+        public abstract class BaseRepository
+        {
+            protected readonly AppDbContext _context;
+
+            public BaseRepository(AppDbContext context)
+            {
+                _context = context;
+            }
+        }
+    }
+
